fed30b7f396ec872f874ed296cc0ba0f094cce34
肖然 <ryanshaw1992@163.com>
2023-11-07 15:08:12 +0800

    手机是否注册接口

---
+++ app/http/controllers/api/v1/base_api_controller.go
@@ -0,0 +1,5 @@
+package v1
+
+// BaseApiController基础控制器
+type BaseAPIController struct {
+}
--- routes/api.go
+++ routes/api.go
@@ -2,7 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"gohub/app/http/controllers/api/v1/auth"
 )
 
 // 注册网页相关路由
@@ -10,12 +10,10 @@ func RegisterApiRoutes(r *gin.Engine) {
 	//V1版本的路由
 	v1 := r.Group("/v1")
 	{
-		//注册一个路由
-		v1.GET("/", func(c *gin.Context) {
-			//以json格式响应
-			c.JSON(http.StatusOK, gin.H{
-				"Hello": "World!",
-			})
-		})
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
 	}
 }
