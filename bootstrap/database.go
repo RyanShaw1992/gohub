3012371e8fb214b4a027d967ebc560f25d9d52eb
肖然 <ryanshaw1992@163.com>
2023-11-16 10:23:33 +0800

    Panic Recovery

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -3,7 +3,6 @@ package bootstrap
 import (
 	"errors"
 	"fmt"
-	"gohub/app/models/user"
 	"gohub/pkg/config"
 	"gohub/pkg/database"
 	"gorm.io/driver/mysql"
@@ -50,5 +49,5 @@ func SetupDB() {
 	database.SQLDB.SetConnMaxLifetime(time.Duration(config.GetInt("database.mysql.max_life_seconds")) * time.Second)
 
 	//数据库自动迁移
-	database.DB.AutoMigrate(&user.User{})
+	//database.DB.AutoMigrate(&user.User{})
 }
--- bootstrap/route.go
+++ bootstrap/route.go
@@ -19,7 +19,7 @@ func SetupRoute(router *gin.Engine) {
 }
 
 func registerGlobalMiddleware(router *gin.Engine) {
-	router.Use(middlewares.Logger(), gin.Recovery())
+	router.Use(middlewares.Logger(), middlewares.Recovery())
 }
 
 func setup404Handler(router *gin.Engine) {
