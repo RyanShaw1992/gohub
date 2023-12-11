5c2cc1159c1bdd54f8470dce5fe043ab205b2b20
肖然 <ryanshaw1992@163.com>
2023-12-11 13:48:54 +0800

    response包

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -5,7 +5,7 @@ import (
 	v1 "gohub/app/http/controllers/api/v1"
 	"gohub/app/models/user"
 	"gohub/app/requests"
-	"net/http"
+	"gohub/pkg/response"
 )
 
 // SignupController注册控制器
@@ -22,7 +22,7 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 	}
 
 	//检查数据库并返回响应
-	c.JSON(http.StatusOK, gin.H{
+	response.JSON(c, gin.H{
 		"exist": user.IsPhoneExist(request.Phone),
 	})
 }
@@ -36,7 +36,7 @@ func (sc *SignupController) IsEmailExist(c *gin.Context) {
 	}
 
 	//  检查数据库并返回响应
-	c.JSON(http.StatusOK, gin.H{
+	response.JSON(c, gin.H{
 		"exist": user.IsEmailExist(request.Email),
 	})
 }
--- app/http/controllers/api/v1/auth/verify_code_controller.go
+++ app/http/controllers/api/v1/auth/verify_code_controller.go
@@ -5,7 +5,7 @@ import (
 	v1 "gohub/app/http/controllers/api/v1"
 	"gohub/pkg/captcha"
 	"gohub/pkg/logger"
-	"net/http"
+	"gohub/pkg/response"
 )
 
 // VerifyCodeController用户控制器
@@ -20,7 +20,7 @@ func (vc *VerifyCodeController) ShowCaptcha(c *gin.Context) {
 	//记录错误日志，因为验证码是用户的入口，出错时应该记录error等级的日志
 	logger.LogIf(err)
 	//返回给用户
-	c.JSON(http.StatusOK, gin.H{
+	response.JSON(c, gin.H{
 		"captcha_id":    id,
 		"captcha_image": b64s,
 	})
